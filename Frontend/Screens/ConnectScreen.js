dc5e8bc3cf1558874d81ec14cfdd176de3dfabbe
AJ5831A <jakhararyan27@gmail.com>
2025-04-05 03:09:57 +0530

    Added all the base Screens to work for now on DrawerNavigation

---
+++ Frontend/Screens/ConnectScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ConnectScreen = () => {
+  return (
+    <View>
+      <Text>ConnectScreen</Text>
+    </View>
+  )
+}
+
+export default ConnectScreen
+
+const styles = StyleSheet.create({})
