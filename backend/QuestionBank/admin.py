7730f06c06515093153ba323293be524096f655f
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-04-18 00:28:31 +0530

    QuestionBank is fixed as I lost the code

--- backend/QuestionBank/admin.py
+++ backend/QuestionBank/admin.py
@@ -1,10 +1,12 @@
-from django.contrib import admin # type: ignore
+from django.contrib import admin  # type: ignore
 from .models import Topic, Question, QuestionChoice
+
+from .models import Topic, Question, QuestionChoice, QuestionBank
 
 
 class QuestionChoiceInline(admin.TabularInline):
     model = QuestionChoice
-    extra = 4  
+    extra = 4
     fields = ["choice_text", "is_correct", "matching_text", "order"]
 
 
@@ -79,6 +81,47 @@ class QuestionAdmin(admin.ModelAdmin):
     get_topics.short_description = "Topics"
 
     def save_model(self, request, obj, form, change):
-        if not change: 
+        if not change:
             obj.added_by = request.user
         super().save_model(request, obj, form, change)
+
+
+@admin.register(QuestionBank)
+class QuestionBankAdmin(admin.ModelAdmin):
+    list_display = ("name", "question_count", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    filter_horizontal = ("questions",)
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+        (
+            "Bank Information",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                )
+            },
+        ),
+        (
+            "Questions",
+            {
+                "fields": ("questions",),
+            },
+        ),
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    def question_count(self, obj):
+        return obj.questions.count()
+
+    question_count.short_description = "Number of Questions"
